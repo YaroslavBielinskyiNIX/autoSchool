cc7eef4ced1a0d03686561006611886fb786bcd6
Yaroslav Bielinskyi <yaroslav.bielinskyi@nixsolutions.com>
2020-05-25 12:01:29 +0300

    Little fix

--- module11_2/src/main/java/AutomationPractice.java
+++ module11_2/src/main/java/AutomationPractice.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -8,8 +9,10 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.Integer.parseInt;
+import static matchers.InnerHtmlMatcher.hasInnerHtml;
+import static matchers.StringMatcher.hasText;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.openqa.selenium.By.xpath;
-import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class AutomationPractice {
@@ -23,7 +26,7 @@ public class AutomationPractice {
 
     @BeforeMethod
     public void init() {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Yaroslav\\IdeaProjects\\autoSchool\\module11_2\\src\\main\\resources\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
         webDriver = new ChromeDriver();
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
@@ -34,35 +37,40 @@ public class AutomationPractice {
         webDriver.findElement(xpath("//input[@id='search_query_top']")).sendKeys(searchQuery);
         webDriver.findElement(xpath("//button[@name='submit_search']")).click();
 
-        assertTrue(webDriver.findElement(xpath("//span[@class='lighter']")).getText().toLowerCase().contains(searchQuery.toLowerCase()));
+        assertThat(webDriver.findElement(xpath("//span[contains(@class,'lighter')]")), hasText(searchQuery));
 
         Select dropDownMenu = new Select(webDriver.findElement(xpath("//select[@id='selectProductSort']")));
         dropDownMenu.selectByVisibleText("Price: Highest first");
 
-        List<WebElement> items = webDriver.findElements(xpath("//ul[@class='product_list grid row']/li/div"));
-        String nameItem = items.get(0).findElement(xpath("//div[@class='right-block']//a[@class='product-name']")).getText();
+        List<WebElement> items = webDriver.findElements(xpath("//ul[contains(@class,'product_list grid row')]/li/div"));
+        String nameItem = items.get(0).findElement(xpath("//div[contains(@class,'right-block')]//a[contains(@class,'product-name')]")).getText();
         String priceItem;
 
         try {
-            priceItem = items.get(0).findElement(xpath("//span[@class='old-price product-price']")).getAttribute("innerHTML").trim();
+            priceItem = items.get(0).findElement(xpath("//span[contains(@class,'old-price product-price')]")).getAttribute("innerHTML").trim();
         } catch (Exception e) {
-            priceItem = items.get(0).findElement(xpath("//span[@class='price product-price']")).getAttribute("innerHTML").trim();
+            priceItem = items.get(0).findElement(xpath("//span[contains(@class,'price product-price')]")).getAttribute("innerHTML").trim();
         }
 
-        if (items.size() > 1) assertTrue(parseInt(priceItem.replaceAll("[^0-9]","")) >= parseInt(items.get(1).findElement(xpath("//span[@class='old-price product-price']")).getAttribute("innerHTML").trim().replaceAll("[^0-9]","")));
+        if (items.size() > 1) assertTrue(parseInt(priceItem.replaceAll("[^0-9]","")) >= parseInt(items.get(1).findElement(xpath("//span[contains(@class,'old-price product-price')]")).getAttribute("innerHTML").trim().replaceAll("[^0-9]","")));
 
         items.get(0).findElement(xpath("//a[@title='Add to cart']")).click();
         webDriver.findElements(xpath("//span[contains(text(),'Proceed to checkout')]")).get(0).click();
 
         WebElement priceItemCart;
         try {
-            priceItemCart = webDriver.findElement(xpath("//span[@class='old-price']"));
+            priceItemCart = webDriver.findElement(xpath("//span[contains(@class,'old-price')]"));
         } catch (Exception e) {
-            priceItemCart = webDriver.findElement(xpath("//td[@class='cart_unit']/span[@class='price']"));
+            priceItemCart = webDriver.findElement(xpath("//span/span[contains(@class,'price')]"));
         }
 
-        assertEquals(priceItemCart.getText(), priceItem);
-        assertEquals(webDriver.findElement(xpath("//div/p[@class='product-name']/a")).getAttribute("innerHTML").trim(), nameItem);
+        assertThat(priceItemCart, hasText(priceItem));
+        assertThat(webDriver.findElement(xpath("//td/p[contains(@class,'product-name')]/a")), hasInnerHtml(nameItem));
+    }
+
+    @AfterMethod
+    public void tearDownAfterMethod() {
+        webDriver.close();
     }
 
     @AfterTest
