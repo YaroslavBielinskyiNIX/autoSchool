acf8e66c2b95caa855b032274c01edda9162a3fc
Yaroslav Bielinskyi <yaroslav.bielinskyi@nixsolutions.com>
2020-06-03 15:21:02 +0300

    GSON added

--- bookingTask/src/test/java/TestClass.java
+++ bookingTask/src/test/java/TestClass.java
@@ -23,31 +23,4 @@ public class TestClass {
                 .bookRoom()
                 .deleteBook();
     }
-
-    @Test
-    public void test() {
-        JSONObject jsonObj = new JSONObject().put("data", new JSONObject()
-                .put("attributes", new JSONObject()
-                        .put("description", "")
-                        .put("end-time", "2020-06-16T10:05:00")
-                        .put("id", "")
-                        .put("recurring-event-id", "")
-                        .put("start-time", "2020-06-16T09:05:00")
-                        .put("title", ""))
-                .put("id", "")
-                .put("relationships", new JSONObject()
-                        .put("room", new JSONObject()
-                                .put("data", new JSONObject()
-                                        .put("id", "9")
-                                        .put("type", "rooms"))))
-                .put("type", "events"));
-
-        String string = "{\"data\":{\"relationships\":{\"room\":{\"data\":{\"id\":\"9\",\"type\":\"rooms\"}}},\"attributes\":{\"start-time\":\"2020-06-16T09:05:00\",\"end-time\":\"2020-06-16T10:05:00\",\"description\":\"\",\"id\":\"\",\"title\":\"\",\"recurring-event-id\":\"\"},\"id\":\"\",\"type\":\"events\"}}\n";
-
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
-
-        BookRequestInfo bookRequestInfo = gson.fromJson(string, BookRequestInfo.class);
-        System.out.println(gson.toJson(bookRequestInfo));
-    }
 }
---
+++ bookingTask/src/test/java/steps/BookResponseInfo.java
@@ -0,0 +1,39 @@
+package steps;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+
+@Getter
+public class BookResponseInfo {
+
+    private Data data;
+
+    @Getter
+    public class Data {
+
+        private Attributes attributes;
+
+        @Getter
+        public class Attributes {
+
+            @SerializedName("room-id")
+            private String roomId;
+
+            @SerializedName("recurring-event-id")
+            private String recurringEventId;
+
+            @SerializedName("start-time")
+            private String startTime;
+
+            @SerializedName("end-time")
+            private String endTime;
+
+            @SerializedName("is-deleted")
+            private String isDeleted;
+
+            private String id;
+            private String description;
+            private String title;
+        }
+    }
+}
